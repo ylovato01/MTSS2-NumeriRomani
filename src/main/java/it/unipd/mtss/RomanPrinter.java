27fa7fa94c28046347f30c3c637ea3716c51ee77
unknown <y.lovato01@gmail.com>
2023-05-17 12:39:22 +0200

    Close #4

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -3,11 +3,6 @@
 // Yuri Lovato 1238100
 ////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
-
-public class App 
+public class RomanPrinter
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
 }

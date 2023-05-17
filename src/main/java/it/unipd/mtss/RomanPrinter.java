a11bdc51e4ba44788c8eef3764f86f46be8f43b1
unknown <y.lovato01@gmail.com>
2023-05-17 13:01:12 +0200

    Close #7

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -5,4 +5,12 @@
 package it.unipd.mtss;
 public class RomanPrinter
 {
+    public static String print(int num)
+    {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+     private static String printAsciiArt(String romanNumber)
+    {
+        return "";
+    }
 }

6ec3f7fb16ce035b4d5687b0ee01a596e95dee06
Alcky26 <Alchy.26.07@gmail.com>
2023-05-17 11:41:36 +0200

    Close #2

--- src/test/java/it/unipd/mtss/AppTest.java
+++ src/test/java/it/unipd/mtss/AppTest.java
@@ -1,17 +1,15 @@
+////////////////////////////////////////////////////////////////////
+// Matteo Vignaga 1201147
+// Yuri Lovato 1238100
+////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
